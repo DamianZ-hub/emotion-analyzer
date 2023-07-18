3cd9f7e62464e8ce00a3998ea9779f9d5fb947c7
damia <damian.zam1f20@gmail.com>
2023-07-18 18:45:41 +0200

    .

---
+++ src/test/java/com/example/emotionanalyzer/EmotionAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emotionanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
